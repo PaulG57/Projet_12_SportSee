35b4430cadb2cbcc367293efed4e5d50df1baa48
Paul Glatt <paul.glatt57@gmail.com>
2025-02-16 21:15:22 +0100

    icon sidebar

--- src/components/PerformanceChart.jsx
+++ src/components/PerformanceChart.jsx
@@ -4,10 +4,10 @@ import "../styles/performance.css";
 const RadarGraph = ({ data }) => {
     return (
         <div className="radar-container">
-            <ResponsiveContainer width="100%" height="100%">
+            <ResponsiveContainer>
                 <RadarChart cx="50%" cy="50%" outerRadius="70%" data={data}>
-                    <PolarGrid stroke="white" strokeOpacity={0.7} radialLines={false} polarRadius={[10, 20, 40, 60, 80]} />
-                    <PolarAngleAxis dataKey="kind" tick={{ fill: "white", fontSize: 12 }} tickSize={10} />
+                    <PolarGrid stroke="white" strokeOpacity={0.7} radialLines={false} />
+                    <PolarAngleAxis dataKey="kind" tick={{ fill: "white", fontSize: 12 }} tickSize={15} />
                     <Radar dataKey="value" stroke="red" fill="red" fillOpacity={0.6} />
                 </RadarChart>
             </ResponsiveContainer>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,20 @@
+import icon1 from "../assets/icon_1.png";
+import icon2 from "../assets/icon_2.png";
+import icon3 from "../assets/icon_3.png";
+import icon4 from "../assets/icon_4.png";
+
 function Sidebar() {
     return (
       <aside className="sidebar">
         <ul>
-          <li>🏋️‍♂️</li>
-          <li>🚴‍♂️</li>
-          <li>🧘‍♂️</li>
-          <li>🏊‍♂️</li>
+          <li><img src={icon1} alt="Yoga" /></li>
+          <li><img src={icon2} alt="Natation" /></li>
+          <li><img src={icon3} alt="Vélo" /></li>
+          <li><img src={icon4} alt="Musculation" /></li>
         </ul>
         <p>Copyright SportSee 2025</p>
       </aside>
     );
-  }
-  
-  export default Sidebar;
-  
+}
+
+export default Sidebar;
